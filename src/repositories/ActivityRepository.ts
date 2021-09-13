687d9f01eb869b8ed949c77734842af85d436a21
Emanuel Vieira <51376864+emanuelvs@users.noreply.github.com>
2021-09-12 22:35:24 -0300

    Update ActivityRepository.ts

--- src/repositories/ActivityRepository.ts
+++ src/repositories/ActivityRepository.ts
@@ -1,6 +1,6 @@
 import Knex from "knex";
 import { DBActivity } from "../db";
-import { Repository } from "./repository";
+import { Repository } from "./Repository";
 
 export class ActivityRepository extends Repository<DBActivity> {
   constructor(knex: Knex) {
